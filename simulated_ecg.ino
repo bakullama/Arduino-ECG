a8370266b0a23570ee68f09d8541bd96a82d5156
Finlay Haggar <fmh@finlaymh.uk>
2021-02-04 16:25:23 +0000

    adding so i can test without having to use up electrodes

---
+++ simulated_ecg.ino
@@ -0,0 +1,76 @@
+#include <SAMDUETimerInterrupt.h>
+#include <SAMDUE_ISR_Timer.h>
+#define HALF_MINUTE_MS 30000
+#define FULL_MINUTE_MS 60000
+
+int currentval = 0;
+int thresholdcount = 0;
+
+bool disp = true;
+
+float beats = 0;
+float minuteBeats = 0;
+double bpm;
+double minutesPassed;
+bool halfMinuteTrigger = false;
+bool minuteTrigger = false;
+
+void handleHalfMinute(void) {
+  halfMinuteTrigger = true;
+}
+
+void handleMinute(void) {
+  minuteTrigger = true;
+}
+
+uint16_t attachDueInterrupt(double microseconds, timerCallback callback) {
+  DueTimerInterrupt dueTimerInterrupt = DueTimer.getAvailable();
+  dueTimerInterrupt.attachInterruptInterval(microseconds, callback);
+  uint16_t timerNumber = dueTimerInterrupt.getTimerNumber();
+  return timerNumber;
+}
+
+
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(115200);
+  pinMode(10, INPUT); // Setup for leads off detection LO +
+  pinMode(11, INPUT); // Setup for leads off detection LO -
+
+  attachDueInterrupt(HALF_MINUTE_MS * 1000, handleHalfMinute);
+  attachDueInterrupt(FULL_MINUTE_MS * 1000, handleMinute);
+
+
+}
+
+void loop() {
+  if (halfMinuteTrigger) {
+    if (disp) {
+      double multiplier = (millis() / 1000) / (double)60;
+      bpm = minuteBeats * 2;
+      Serial.print("Current HR: ");
+      Serial.print(bpm);
+      Serial.println(" bpm");
+    }
+    halfMinuteTrigger = false;
+    disp = !disp;
+    
+  } if (minuteTrigger) {
+    minutesPassed++;
+    Serial.print("HR avg: ");
+    Serial.print(beats/minutesPassed);
+    Serial.println(" bpm");
+    minuteBeats = 0;
+    minuteTrigger = false;
+    
+
+    
+  } else { // Generate random delay then increment beat
+    int delayms = random(10, 20)
+    delay(delayms);
+    beats++;
+  }
+    
+  //Wait for a bit to keep serial data from saturating
+  delay(1);
+}
