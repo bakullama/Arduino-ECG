ba5b5ee77dd488600849b5826029aed71427ea1c
Finlay Haggar <fmh@finlaymh.uk>
2021-02-05 21:54:58 +0000

    removed half second interrupt, and added LED at pin 13

--- ecg.ino
+++ ecg.ino
@@ -1,7 +1,6 @@
 #include <SAMDUETimerInterrupt.h>
 #include <SAMDUE_ISR_Timer.h>
 #include <CircularBuffer.h>
-#define HALF_MINUTE_MS 30000
 #define FULL_MINUTE_MS 60000
 #define SECOND_MS 1000
 #define BUFFER_SIZE 60
@@ -9,22 +8,18 @@
 int currentval = 0;
 int thresholdcount = 0;
 
-float beats = 0;
-float minuteBeats = 0;
-int waittime;
-double bpm;
-double minutesPassed;
-bool halfMinuteTrigger = false;
-bool minuteTrigger = false;
-bool secondTrigger = false;
+int beats = 0;
+int minuteBeats = 0;
+int minutesPassed = 0;
+volatile bool minuteTrigger = false;
+volatile bool secondTrigger = false;
+int startTime = 0;
 
 CircularBuffer<int,BUFFER_SIZE> secondBuffer;
 int bufferSum = 0;
 int secondBeats = 0;
 
-void handleHalfMinute(void) {
-  halfMinuteTrigger = true;
-}
+// ISRs
 
 void handleMinute(void) {
   minuteTrigger = true;
@@ -33,6 +28,7 @@ void handleMinute(void) {
 void handleSecond(void) {
   secondTrigger = true;
 }
+
 
 uint16_t attachDueInterrupt(double microseconds, timerCallback callback) {
   DueTimerInterrupt dueTimerInterrupt = DueTimer.getAvailable();
@@ -43,22 +39,26 @@ uint16_t attachDueInterrupt(double microseconds, timerCallback callback) {
 
 
 void setup() {
-  // initialize the serial communication:
+
   Serial.begin(115200);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
-
-  attachDueInterrupt(HALF_MINUTE_MS * 1000, handleHalfMinute);
-  Serial.println("Half minute interrupt setup\tOK");
+  pinMode(13, OUTPUT); // indicator LED
+  digitalWrite(13, HIGH);
   attachDueInterrupt(FULL_MINUTE_MS * 1000, handleMinute);
   Serial.println("Full minute interrupt setup\tOK");
   attachDueInterrupt(SECOND_MS * 1000, handleSecond);
-  Serial.println("Second interrupt setup\tOK"
+  Serial.println("Second interrupt setup\tOK");
+  
+  delay(500);
   Serial.println("Starting, first reading will come after 30 seconds, and accurate readings from 60 seconds");
-
+  digitalWrite(13, LOW);
 }
 
 void loop() {
+  if (millis() - startTime >= 20) {
+    digitalWrite(13, LOW);
+  }
   if (minuteTrigger) {
     minutesPassed++;
     Serial.print("HR avg: ");
@@ -66,15 +66,9 @@ void loop() {
     Serial.println(" bpm");
     minuteBeats = 0;
     minuteTrigger = false;
-  
-  } if (halfMinuteTrigger) {
-    double multiplier = (millis() / 1000) / (double)60;
-    bpm = minuteBeats * 2;
-    Serial.print("Current HR: ");
-    Serial.print(bpm);
-    Serial.println(" bpm");
-    halfMinuteTrigger = false;
-  } if (secondTrigger) {
+
+  } 
+  if (secondTrigger) {
     if (secondBuffer.isFull()){
     bufferSum = 0;
     secondBuffer.unshift(secondBeats);
@@ -96,14 +90,17 @@ void loop() {
       thresholdcount++;
     } else if (thresholdcount > 0 && currentval < 1000){
       if (thresholdcount > 20) {
-//        Serial.println("beat");
+        digitalWrite(13, HIGH);
+        startTime = millis();
         secondBeats++;
         beats+=1;    
         minuteBeats+=1;
+        
       }
       thresholdcount = 0;
     }
    }
 //Wait for a bit to keep serial data from saturating
   delay(1);
+  
 }
